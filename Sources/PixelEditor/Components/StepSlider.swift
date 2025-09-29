8e2f47df8b121d87c7dcb8892b9a4066c437f6a2
Hristo Hristov <hristo.hristov@we.shop>
2025-09-29 11:38:50 +0300

    Fixed Style's module and bumped version

--- Sources/PixelEditor/Components/StepSlider.swift
+++ Sources/PixelEditor/Components/StepSlider.swift
@@ -315,7 +315,7 @@ private final class _StepSlider: UISlider {
     minimumTrackTintColor = UIColor.clear
     maximumTrackTintColor = UIColor.clear
     setThumbImage(UIImage(named: "slider_thumb", in: bundle, compatibleWith: nil), for: [])
-    tintColor = Style.default.black
+    tintColor = PixelEditor.Style.default.black
 
     let label = stepLabel
     label.backgroundColor = UIColor.clear
